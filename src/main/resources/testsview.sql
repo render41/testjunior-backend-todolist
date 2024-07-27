dfb8e28c5d93be174d7cdaaa08c355d0e0cc6b57
render41 <gabrielservice05@gmail.com>
2024-07-27 16:36:36 -0300

    Create my first project backend

---
+++ src/main/resources/testsview.sql
@@ -0,0 +1,57 @@
+{
+    "type": "MySQLNotebook",
+    "version": "1.0",
+    "caption": "testsview",
+    "content": "",
+    "options": {
+        "tabSize": 4,
+        "indentSize": 4,
+        "insertSpaces": true,
+        "defaultEOL": "LF",
+        "trimAutoWhitespace": true
+    },
+    "viewState": null,
+    "contexts": [
+        {
+            "state": {
+                "start": 1,
+                "end": 1,
+                "language": "mysql",
+                "result": {
+                    "type": "text",
+                    "text": [
+                        {
+                            "type": 4,
+                            "index": 0,
+                            "resultId": "a4123a04-ede5-4c20-b3ce-2bd8fe82b0f3",
+                            "content": "OK, 0 records retrieved in 0.246ms"
+                        },
+                        {
+                            "type": 0,
+                            "index": 1,
+                            "content": "MySQL Error (1064): ClassicSession.run_sql: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 1",
+                            "language": "ansi"
+                        }
+                    ],
+                    "executionInfo": {
+                        "text": ""
+                    }
+                },
+                "currentHeight": 299.984375,
+                "currentSet": 1,
+                "statements": [
+                    {
+                        "delimiter": ";",
+                        "span": {
+                            "start": 0,
+                            "length": 0
+                        },
+                        "contentStart": 0,
+                        "state": 0
+                    }
+                ]
+            },
+            "data": []
+        }
+    ]
+}
