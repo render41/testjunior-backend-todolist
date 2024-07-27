23c6dd591409c3a0e682918b00d48e06b8ce1bee
render41 <gabrielservice05@gmail.com>
2024-07-27 15:20:21 -0300

    send project default

---
+++ src/main/java/br/com/gabrielmiranda/_1/testjunior_todolist/TestjuniorTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmiranda._1.testjunior_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjuniorTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjuniorTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrielmiranda/_1/testjunior_todolist/TestjuniorTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmiranda._1.testjunior_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjuniorTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
